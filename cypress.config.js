997a415b6ba2b8ff09b0c7517045f106aa2fc785
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-06-06 13:37:17 -0400

    solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,33 @@
 const { defineConfig } = require('cypress')
 // https://github.com/bahmutov/cypress-split
 const cypressSplit = require('cypress-split')
+const path = require('path')
+
+// TODO: add the "reset-spec.js" to the specs
+// and always run it first
+function changeSpecOrder(specs) {
+  // we must deal with absolute file paths
+  const resetSpec = path.resolve('cypress/e2e/reset-spec.js')
+  if (specs.includes(resetSpec)) {
+    specs = specs.filter((s) => s !== resetSpec)
+  }
+  // put the reset spec first
+  specs.unshift(resetSpec)
+  console.log('split spec order')
+  specs.forEach((spec) => {
+    console.log(spec)
+  })
+
+  return specs
+}
 
 module.exports = defineConfig({
   fixturesFolder: false,
   video: false,
   e2e: {
-    env: {
-      grepFilterSpecs: true,
-      grepOmitFiltered: true,
-    },
     setupNodeEvents(on, config) {
-      cypressSplit(on, config)
-
-      require('@bahmutov/cy-grep/src/plugin')(config)
-      // make sure to return the config object
-      // as it might have been modified by the plugin
+      cypressSplit(on, config, changeSpecOrder)
+      // IMPORTANT: return the modified config object
       return config
     },
     baseUrl: 'http://localhost:8888',
---
+++ cypress/e2e/reset-spec.js
@@ -0,0 +1,13 @@
+// type definitions for Cypress and
+// custom commands like "createDefaultTodos"
+/// <reference types="../support" />
+
+// check this file using TypeScript if available
+// @ts-check
+
+describe('TodoMVC - React', function () {
+  it('resets the data', () => {
+    // pretend that this spec should run before other specs
+    cy.visit('/')
+  })
+})
