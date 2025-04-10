77baa52bd3a820f3e750a24761cf419e563210a0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-04-10 17:34:30 -0400

    bump dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -1,27 +1,13 @@
 const { defineConfig } = require('cypress')
 // https://github.com/bahmutov/cypress-split
 const cypressSplit = require('cypress-split')
-const path = require('path')
-
-// TODO: add the "reset-spec.js" to the specs
-// and always run it first
-function changeSpecOrder(specs) {
-  // we must deal with absolute file paths
-  // put the reset spec first
-  console.log('split spec order')
-  specs.forEach((spec) => {
-    console.log(spec)
-  })
-
-  return specs
-}
 
 module.exports = defineConfig({
   fixturesFolder: false,
   video: false,
   e2e: {
     setupNodeEvents(on, config) {
-      cypressSplit(on, config, changeSpecOrder)
+      cypressSplit(on, config)
       // IMPORTANT: return the modified config object
       return config
     },
